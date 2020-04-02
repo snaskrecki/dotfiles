06d12e8f105a26eb606bb91155a8783de9231b5f
Szymon Naskrecki <snaskrecki@gmail.com>
2020-04-02 03:54:44 +0200

    first entry

---
+++ create-links.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/Code/User
+mkdir -p $HOME/.config/i3
+
+DOTFILES=$(pwd)
+
+ln -nfs $DOTFILES/bashrc $HOME/.bashrc
+ln -nfs $DOTFILES/gitconfig $HOME/.gitconfig
+ln -nfs $DOTFILES/vim $HOME/.vim
+ln -nfs $DOTFILES/vim/viminfo $HOME/.viminfo
+ln -nfs $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -nfs $DOTFILES/nvim $HOME/.config/nvim
+ln -nfs $DOTFILES/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln -nfs $DOTFILES/i3/config $HOME/.config/i3/config
---
+++ vim/after/ftplugin/cpp.vim
@@ -0,0 +1,6 @@
+" setlocal shiftwidth=2
+" setlocal tabstop=2
+
+"" Overwriting settings in vimrc
+" setlocal expandtab
+" setlocal softtabstop=2
