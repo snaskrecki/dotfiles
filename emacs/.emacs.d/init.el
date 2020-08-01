8a871e5080e2714d2331c02494946b6ea7a6ed05
Szymon Naskrecki <snaskrecki@gmail.com>
2020-08-01 22:16:47 +0200

    Add Emacs Org mode settings

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,35 @@
-;; Set a Color Theme Permanently
+;; ---------------------------------------------------------------------------
+;; org-mode
+;; ---------------------------------------------------------------------------
+
+;; -*- mode: elisp -*-
+
+;; Disable the splash screen (to enable it agin, replace the t with 0)
+(setq inhibit-splash-screen t)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;Org mode configuration
+;; Enable Org mode
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+;; Make Org mode work with files ending in .org
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is the default in recent emacsen;; Set a Color Theme Permanently
+
+;; add workflow states for todos
+(setq org-todo-keywords
+  '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+;; ---------------------------------------------------------------------------
+;; color theme
+;; ---------------------------------------------------------------------------
+
+;; set permanent color theme
 (load-theme 'misterioso)
 
 ;; disable emacs's automatic backup~ file
@@ -6,3 +37,17 @@
 
 ;; stop creating those #auto-save# files
 (setq auto-save-default nil)
+
+;; enable line numbers
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
+;; insert a space after line number
+(setq linum-format "%d ")
+
+;; disable emacs's automatic backup ~ file
+(setq make-backup-files nil)
+
+;; stop creating those #auto-save# files
+(setq auto-save-default nil)
+
